636334b803ab746bfb8f4a6a14d723b34fa4b666
gang.cao <gang.cao@hobot.cc>
2017-03-31 14:58:13 +0800

    label的shape应该是（batchSize，）

--- src/discriminator/DiscriminatorDataIter.py
+++ src/discriminator/DiscriminatorDataIter.py
@@ -30,7 +30,7 @@ class DiscriminatorDataIter(mx.io.DataIter):
         self.initStates = initStates
         self.initStateArrays = [mx.nd.zeros(x[1]) for x in initStates]
         self.provide_data = [('inputData', (self.batchSize, inputSeqLen)), ('outputData',(self.batchSize, outputSeqLen))] + initStates
-        self.provide_label = [('softmaxLabel', (self.batchSize, 2))]
+        self.provide_label = [('softmaxLabel', (self.batchSize,))]
         self.curr_idx = 0
         self.batches = self.discriminatorData.getBatches()
 
