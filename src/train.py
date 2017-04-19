903fb5a22c67f9baed8fdd8f0e6d16a147f36a83
gang.cao <gang.cao@hobot.cc>
2017-04-19 13:29:27 +0800

    Fixed : GroupAccuracy函数定义出错

--- src/train.py
+++ src/train.py
@@ -30,7 +30,7 @@ class GroupAccuracy(mx.metric.EvalMetric):
             self.sum_metric += (pred_label.flat == label.flat).sum()
             self.num_inst += len(pred_label.flat)
 
-    def ndarray_argmax(pred):  # argmax the last dim
+    def ndarray_argmax(self, pred):  # argmax the last dim
         if len(pred.shape) > 2:
             flatten_shape = (np.prod(pred.shape[0:-1]), pred.shape[-1])
             pred_label = mx.ndarray.argmax_channel(pred.reshape(flatten_shape))
